5399d6d829742590c9dc29ef113e36c651a508a5
virus29 <aaaaa55555@ro.ru>
2016-07-21 16:09:20 +0300

    Lesson3/snake2

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -10,22 +10,20 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1,y1,sym1);
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            point p2 = new point();
+            p1.x = 4;
+            p1.y = 5;
+            p1.sym = '#';
+            Draw();
+                       
 
             Console.ReadLine();
-        }
-        static void Draw(int x, int y, char sys)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
