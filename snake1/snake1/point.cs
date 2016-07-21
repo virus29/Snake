1664153f0b871a0bcf74659a4a8c8d8d125de547
virus29 <aaaaa55555@ro.ru>
2016-07-21 16:17:47 +0300

    Lesson3/snake2

---
+++ snake1/snake1/point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake1
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(sym);
+        }
+    }
+}
